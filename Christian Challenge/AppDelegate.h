1121f35d05001523c3938555488bed62986b9d36
Angel Olivera <angelolivera331@Angel-Oliveras-MacBook-Pro.local>
2013-05-01 17:16:52 -0400

    Initial Commit

---
+++ Christian Challenge/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Christian Challenge
+//
+//  Created by Angel Olivera on 5/1/13.
+//  Copyright (c) 2013 Angel Olivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
